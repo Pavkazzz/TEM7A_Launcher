9f77f8e1fe2242bda7d95995dfd8c72dfd47b107
Сёма Байков <cemkaxd2@live.com>
2014-12-15 16:01:24 +0300

    Список ГОСТОВ

--- Launcher/DocumentModule/MainWindowControl.xaml.cs
+++ Launcher/DocumentModule/MainWindowControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace DocumentModule
 {
@@ -20,17 +21,21 @@ namespace DocumentModule
     /// </summary>
     public partial class MainWindowControl : UserControl
     {
+        public static readonly string ResourcePath = System.IO.Path.GetFullPath(@"../../Resources");
+        public static readonly string ModulesPath = System.IO.Path.GetFullPath(@"../../Modules");
+        private SQLiteConnection _sqLiteConnectionDatabase;
         public MainWindowControl()
         {
             InitializeComponent();
+            ConnectToDB(ResourcePath);
         }
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
             //kostyl из базы забирать категории
-            foreach (string item in new List<string>(new [] {"ГОСТ", "ОСТ", "ТР", "Приказы", "Распоряжения"}))
+            foreach (string item in new List<string>(new[] { "ГОСТ", "ОСТ", "ТР", "Приказы", "Распоряжения" }))
             {
-                var listBoxCategoryItem = new CategoryControl {TextBlockCategory = {Text = item}};
+                var listBoxCategoryItem = new CategoryControl { TextBlockCategory = { Text = item } };
                 ListBoxDocument.Items.Add(listBoxCategoryItem);
             }
         }
@@ -39,13 +44,41 @@ namespace DocumentModule
         {
             //SelectedItemGetText((CategoryControl)sender);
             var lb = ((ListBox)sender);
-            var item = (CategoryControl) lb.SelectedValue;
-            MessageBox.Show(string.Format("Я {0}", item.TextBlockCategory.Text));
+            var item = (CategoryControl)lb.SelectedValue;
+            //MessageBox.Show(string.Format("Я {0}", item.TextBlockCategory.Text));
+            Collection_Create();
         }
 
         public string SelectedItemGetText(CategoryControl categoryControl)
         {
             return categoryControl.TextBlockCategory.Text;
+        }
+        private void ConnectToDB(string path)
+        {   
+            _sqLiteConnectionDatabase = new SQLiteConnection(string.Format(@"Data Source={0}\Normative documents.db",path));
+            _sqLiteConnectionDatabase.Open();
+        }
+
+        public void Collection_Create()
+        {
+            List<ListBoxItem> Coll = new List<ListBoxItem>();
+            SQLiteCommand SqlCommand = new SQLiteCommand("Select NAME_GOST from GOST_TABLE", _sqLiteConnectionDatabase);
+            SQLiteDataReader sqlReader = SqlCommand.ExecuteReader();
+            if (sqlReader.HasRows)
+            {
+                while (sqlReader.Read())
+                {
+                    ListBoxItem a = new ListBoxItem();
+                    a.Content = sqlReader["NAME_GOST"].ToString();
+                    a.FontSize = 18;
+                    //if (sqlReader["Source_To_Document"].ToString() != null)
+                    //{
+                    //    a.Tag = sqlReader["Source_To_Document"].ToString();
+                    //}
+                        Docum.Items.Add(a);
+                }
+
+            }
         }
     }
 }
