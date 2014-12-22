b91419da19514f3bae7f0834d3ace6d79b636608
Pavka <pavkazzz@mail.ru>
2014-12-22 16:57:01 +0300

    na

---
+++ Launcher/Launcher/Modules/Documents/pdf.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentModule;
+using MoonPdfLib;
+using GhostscriptSharp;
+
+namespace Launcher.Modules.Documents
+{
+    class Pdf
+    {
+        static public void ShowPdf(string path)
+        {
+            var dp = new DocumentPresenter();
+            var pdf = new MoonPdfPanel();
+            pdf.OpenFile(path);
+            DatabaseDoc.AddToHistory(path);
+            pdf.ViewType = ViewType.SinglePage;
+            pdf.Zoom(2.0);
+            pdf.PageRowDisplay = PageRowDisplayType.ContinuousPageRows;
+            dp.GridDocument.Children.Add(pdf);
+            dp.ShowDialog();
+            GeneratePageThumb(path);
+        }
+
+        static public void GeneratePageThumb(string path)
+        {
+            GhostscriptWrapper.GeneratePageThumb(path, string.Format("{0}.jpg", path), 1, 500, 500, 500, 300);
+        }
+    }
+}
