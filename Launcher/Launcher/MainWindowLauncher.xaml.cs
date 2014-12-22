15e8eb8c88c3ad52924bcd927aaaee6fd2334def
Сёма Байков <cemkaxd2@live.com>
2014-12-22 10:43:35 +0300

    пустой синк

--- Launcher/Launcher/MainWindowLauncher.xaml.cs
+++ Launcher/Launcher/MainWindowLauncher.xaml.cs
@@ -31,7 +31,7 @@ namespace Launcher
         {
             InitializeComponent();
             StartTimer();
-            TextBlockAny.Text = "Номер локомотива,\n номер поезда \n табельный номер";
+            TextBlockAny.Text = "ЭП10-001\n 062/061 \n табельный номер";
            
         }
 
