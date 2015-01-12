daaf5b6595e7eb253c06d5b7f4a97f83f717ef9f
Сёма Байков <cemkaxd2@live.com>
2015-01-12 17:45:44 +0300

    подстава

--- Launcher/Launcher/Modules/Documents/MainWindowControl.xaml.cs
+++ Launcher/Launcher/Modules/Documents/MainWindowControl.xaml.cs
@@ -65,7 +65,7 @@ namespace DocumentModule
                 a.Content = item;
                 a.FontSize = 18;
                 a.Height = 40;
-                a.Tag = Path.Combine(App.DocPath, item + ".pdf");
+                a.Tag = Path.Combine(App.DocPath, item + ".pdf");//
                 doc.Items.Add(a);
         }
             GridDocument.Children.Add(doc);
