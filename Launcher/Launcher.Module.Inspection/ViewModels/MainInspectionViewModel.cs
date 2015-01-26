6f227b9f43768b3ac7548b817fa0ff1eaab8512e
Pavka <pavkazzz@mail.ru>
2015-01-26 12:50:42 +0000

    viewmodelInspectin

--- Launcher/Launcher.Module.Inspection/ViewModels/MainInspectionViewModel.cs
+++ Launcher/Launcher.Module.Inspection/ViewModels/MainInspectionViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
+using Launcher.Core;
 
 namespace Launcher.Module.Inspection.ViewModels
 {
-    class MainInspectionViewModel
+    class MainInspectionViewModel: Screen, IModule
     {
+        private IEventAggregator _eventAggregator;
+
+        [ImportingConstructor]
+        public MainInspectionViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
     }
 }
