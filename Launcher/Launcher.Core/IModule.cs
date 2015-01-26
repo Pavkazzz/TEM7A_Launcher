f4df02f0199fd1869bd4e08f86f771a9c16d5f92
Pavka <pavkazzz@mail.ru>
2015-01-26 12:46:48 +0000

    template

--- Launcher/Launcher.Core/IModule.cs
+++ Launcher/Launcher.Core/IModule.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 
 namespace Launcher.Core
 {
     [InheritedExport(typeof(IModule))]
-    public interface IModule
+    public interface IModule : IHaveDisplayName
     {
 
     }
--- Launcher/Launcher/ViewModels/LauncherViewModel.cs
+++ Launcher/Launcher/ViewModels/LauncherViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Windows.Controls;
 using Caliburn.Micro;
 using Launcher.Core;
 
@@ -9,25 +10,34 @@ namespace Launcher.ViewModels
 {
 
     [Export(typeof(LauncherViewModel))]
-    class LauncherViewModel : Conductor<IScreen>.Collection.OneActive
+    class LauncherViewModel : Conductor<IScreen>.Collection.OneActive 
     {
+
+        [ImportMany(typeof(IModule))]
+        public IEnumerable<Lazy<IModule>> ModuleList { get; set; }
+
         private IEventAggregator _eventAggregator;
+
+        BindableCollection<string> Modules;
 
         [ImportingConstructor]
         public LauncherViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+            Modules = new BindableCollection<string>();
+            Modules.Add("Модуль нормативные документы");
+           
         }
 
 
-        [ImportMany(typeof(IModule))]
-        public IEnumerable<Lazy<IModule>> getIMain { get; set; }
 
         public void Test()
         {
-
-            ActivateItem(getIMain.ToList()[0].Value as IScreen);
+            ActivateItem(ModuleList.ToList()[0].Value as IScreen);
         }
+
+        
+
     }
 
 }
