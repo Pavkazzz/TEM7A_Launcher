1c2d212e26db73bf9d4f3839ce5254e6f3726274
Pavka <pavkazzz@mail.ru>
2015-01-28 16:00:57 +0000

    test

---
+++ Launcher/Launcher.UnitTest/LauncherUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Launcher.UnitTest
+{
+    [TestFixture]
+    public class LauncherUnitTest
+    {
+        [Test]
+        public void Bootstrapper()
+        {
+            
+        }
+    }
+}
