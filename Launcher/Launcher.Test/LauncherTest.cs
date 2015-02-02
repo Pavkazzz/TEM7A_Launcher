2735cc69798cdb032684f915133c4f2317147c13
Pavka <pavkazzz@mail.ru>
2015-02-02 12:06:23 +0000

    test

---
+++ Launcher/Launcher.Test/LauncherTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Composition.Hosting;
+using System.Windows;
+using Caliburn.Micro;
+using NUnit.Framework;
+using Launcher;
+
+namespace Launcher.Test
+{
+    [TestFixture]
+    public class ExampleTestOfNUnit
+    {
+        
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Shell()
+        {
+            
+        }
+    }
+}
