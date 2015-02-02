a28f4d5b960c8d028bd245ad9eb46e6581560037
Сёма Байков <cemkaxd2@live.com>
2015-02-02 11:09:54 +0300

    s

---
+++ Launcher/Launcher.Core/UserControls/SquareButtonWithRoundedBorders.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Launcher.Core.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для SquareButtonWithRoundedBorders.xaml
+    /// </summary>
+    public partial class SquareButtonWithRoundedBorders : UserControl
+    {
+        public SquareButtonWithRoundedBorders()
+        {
+            InitializeComponent();
+        }
+    }
+}
