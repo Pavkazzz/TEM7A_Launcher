3c23e1ed9a5da1a0839d2f8c159e301c0e57c58a
Pavka <pavkazzz@mail.ru>
2015-03-23 07:24:56 +0000

    module card init

---
+++ Launcher/Launcher.Module.EmergencyCard/EmergencyCardAbout.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Launcher.Core;
+using Launcher.Module.EmergencyCard.ViewModels;
+
+namespace Launcher.Module.EmergencyCard
+{
+    internal class EmergencyCardAbout : IModuleName
+    {
+        public EmergencyCardAbout()
+        {
+            Name = @"Аварийные карточки";
+            Description =
+                @"Модуль «Аварийные карточки» предназначен для ...";
+            ViewModel = typeof (MainEmergencyCardViewModel);
+            //TODO BD
+            DbPath = "../../../../Launcher.Module.EmergencyCard/...";
+        }
+
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Type ViewModel { get; set; }
+        public string DbPath { get; set; }
+        public List<About> Category { get; set; }
+
+        public bool PrimaryCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+}
---
+++ Launcher/Launcher.Module.EmergencyCard/ViewModels/MainEmergencyCardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Launcher.Core;
+using System.ComponentModel.Composition;
+
+namespace Launcher.Module.EmergencyCard.ViewModels
+{
+    [Export(typeof (IModule))]
+    public sealed class MainEmergencyCardViewModel : Conductor<IScreen>.Collection.OneActive, IModule
+    {
+        
+    }
+}
