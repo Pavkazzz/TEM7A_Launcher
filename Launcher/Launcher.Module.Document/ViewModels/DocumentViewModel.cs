cdf4caba72a688124ba09607bf494db1b5735ffd
Pavka <pavkazzz@mail.ru>
2015-03-25 14:58:04 +0000

    add cefsharp

--- Launcher/Launcher.Module.Document/ViewModels/DocumentViewModel.cs
+++ Launcher/Launcher.Module.Document/ViewModels/DocumentViewModel.cs
@@ -30,14 +30,14 @@ namespace Launcher.Module.Document.ViewModels
                 //settings.UserAgent = "CefSharp Browser" + Cef.CefSharpVersion; // Example User Agent
                 //settings.CefCommandLineArgs.Add("renderer-process-limit", "1");
                 //settings.CefCommandLineArgs.Add("renderer-startup-dialog", "renderer-startup-dialog");
-                //settings.CefCommandLineArgs.Add("disable-gpu", "1");
-                //settings.CefCommandLineArgs.Add("disable-gpu-vsync", "1");
+                settings.CefCommandLineArgs.Add("disable-gpu", "1");
+                settings.CefCommandLineArgs.Add("disable-gpu-vsync", "1");
                 //settings.CefCommandLineArgs.Add("enable-media-stream", "1"); //Enable WebRTC
                 //settings.CefCommandLineArgs.Add("no-proxy-server", "1"); //Don't use a proxy server, always make direct connections. Overrides any other proxy server flags that are passed.
 
                 //Disables the DirectWrite font rendering system on windows.
                 //Possibly useful when experiencing blury fonts.
-                //settings.CefCommandLineArgs.Add("disable-direct-write", "1");
+                settings.CefCommandLineArgs.Add("disable-direct-write", "1");
 
                 settings.LogSeverity = LogSeverity.Disable;
 
